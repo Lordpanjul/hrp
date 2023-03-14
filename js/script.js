f2c116165a56b6b1826d6476fa0070a5646a5fa1
Lordpanjul <114191987+Lordpanjul@users.noreply.github.com>
2023-03-14 20:26:28 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// toggle clas active
+function myFunction() {
+  var x = document.getElementById("heru");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
